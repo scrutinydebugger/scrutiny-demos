f9a8f66cd0e0243bdd140c0019e0637c4d8e4e8d
Pier-Yves Lessard <py.lessard@gmail.com>
2025-12-05 19:35:02 -0500

    Update readme

--- stm32f4_cmake/app/toolchain/gcc-arm-none-eabi.cmake
+++ stm32f4_cmake/app/toolchain/gcc-arm-none-eabi.cmake
@@ -38,7 +38,6 @@ set(TARGET_FLAGS "-mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard ")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${TARGET_FLAGS}")
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp -MMD -MP")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -fdata-sections -ffunction-sections")
 
 set(CMAKE_C_FLAGS_DEBUG "-O0 -g3")
 set(CMAKE_C_FLAGS_RELEASE "-Os -g0")
